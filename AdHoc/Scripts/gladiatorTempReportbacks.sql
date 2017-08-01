bb2992bdf32b43f5a5bb75372bbb5da0e5a466d6
Sohaib Hasan <shasan@Sohaibs-MacBook-Pro.local>
2017-08-01 19:08:20 -0400

    reorg

--- AdHoc/Scripts/gladiatorTempReportbacks.sql
+++ AdHoc/Scripts/gladiatorTempReportbacks.sql
@@ -1,4 +1,7 @@
-SELECT s.northstar_id AS "Northstar ID", s.campaign_run_id , count(p.id) AS "number_of_posts"
+SELECT 
+	s.northstar_id AS "Northstar ID", 
+	s.campaign_run_id , 
+	count(p.id) AS "number_of_posts"
 FROM signups s
 LEFT JOIN posts p ON s.id = p.signup_id
 WHERE s.campaign_run_id IN (7811, 7827, 7877, 7825, 7873)
