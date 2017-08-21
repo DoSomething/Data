7ee26921afac8f10fd26c3595d7e4f620f9d517d
shasan <sohaib3@icloud.com>
2017-08-21 17:24:57 -0400

    Functionalized RMySQL connection

--- MAMDive/config/mySQLConfig.R
+++ MAMDive/config/mySQLConfig.R
@@ -1,16 +1,24 @@
 library(RMySQL)
+library(stringr)
 
-pw_file <- '~/Documents/quasar_pw.txt'
+quasarConnect <- function(path) {
+  pw_file <- path
 
-pw <- readChar(pw_file, file.info(pw_file)$size)
+  login <- readChar(pw_file, file.info(pw_file)$size)
+  user <- str_split(login, ':')[[1]][1]
+  pw <- str_split(login, ':')[[1]][2]
 
-con <-
-  dbConnect(
-  MySQL(),
-  user='shasan',
-  host='quasar-slave-new.c9ajz690mens.us-east-1.rds.amazonaws.com',
-  password=pw,
-  dbname='quasar'
-  )
+  con <-
+    dbConnect(
+    MySQL(),
+    user=user,
+    host='quasar-slave-new.c9ajz690mens.us-east-1.rds.amazonaws.com',
+    password=pw,
+    dbname='quasar'
+    )
 
-rm(pw_file, pw)
+  return(con)
+
+}
+
+con <- quasarConnect('~/Documents/quasar_login.txt')
