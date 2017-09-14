a77b4ad5c359ffcb82a3b11e6022f350410548a2
shasan <sohaib3@icloud.com>
2017-09-14 16:56:37 -0400

    greater is different from greater than/equal to

--- AdHoc/Scripts/Nov18_Over18.R
+++ AdHoc/Scripts/Nov18_Over18.R
@@ -7,15 +7,17 @@ q <- "SELECT u.northstar_id, u.birthdate FROM quasar.users u"
 
 dat <- runQuery(q)
 
-dat %>%
+# test <-
+  dat %>%
   mutate(
     birthdate = as.Date(birthdate),
     daysOld = Sys.Date() - birthdate,
-    daysOldInNovember2018 = daysOld + (as.Date('2018-09-30') - Sys.Date()),
-    ageNov2018 = as.numeric(floor(daysOldInNovember2018 / 365))
-  ) %>% 
+    daysOldInNovember2018 = daysOld + (as.Date('2018-11-07') - Sys.Date()),
+    ageNov2018 = as.numeric(floor(daysOldInNovember2018 / 365.25)),
+    ageToday = as.numeric(floor(daysOld / 365.25))
+  ) %>%
   filter(
-    ageNov2018 > 18 & birthdate >= '1930-01-01'
-  ) %>% 
-  summarise(Count = n()) %>% 
+    ageNov2018 >= 18 & ageToday < 18
+  ) %>%
+  summarise(Count = n()) %>%
   as.numeric() -> count
