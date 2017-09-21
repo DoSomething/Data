f3bc52bfc51530661c1fc3985cc3e823c32028b7
shasan <sohaib3@icloud.com>
2017-09-21 09:54:21 -0400

    bug fix

--- AdHoc/Scripts/Nov18_Over18.R
+++ AdHoc/Scripts/Nov18_Over18.R
@@ -3,11 +3,16 @@ source('config/init.R')
 source('config/DSUtils.R')
 source('config/mySQLConfig.R')
 
-q <- "SELECT u.northstar_id, u.birthdate FROM quasar.users u"
+q <- "
+SELECT u.northstar_id, u.birthdate 
+FROM quasar.users u
+WHERE u.customer_io_subscription_status = 'subscribed' 
+OR u.moco_current_status = 'active'
+"
 
 dat <- runQuery(q)
 
-# test <-
+test <-
   dat %>%
   mutate(
     birthdate = as.Date(birthdate),
@@ -15,9 +20,25 @@ dat <- runQuery(q)
     daysOldInNovember2018 = daysOld + (as.Date('2018-11-07') - Sys.Date()),
     ageNov2018 = as.numeric(floor(daysOldInNovember2018 / 365.25)),
     ageToday = as.numeric(floor(daysOld / 365.25))
-  ) %>%
+  ) 
+
+test %>%
   filter(
     ageNov2018 >= 18 & ageToday < 18
   ) %>%
   summarise(Count = n()) %>%
-  as.numeric() -> count
+  as.numeric() -> countTurning18
+
+test %>%
+  filter(
+    ageToday >= 18
+  ) %>%
+  summarise(Count = n()) %>%
+  as.numeric() -> count18
+
+test %>%
+  filter(
+    ageNov2018 >= 18
+  ) %>%
+  summarise(Count = n()) %>%
+  as.numeric() -> countNov18
