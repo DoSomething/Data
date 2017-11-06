1c12311e238a015009045e7bf6737052ad0f73ac
shasan <sohaib3@icloud.com>
2017-11-06 16:32:02 -0500

    Update count

--- AdHoc/Scripts/mel_oct.R
+++ AdHoc/Scripts/mel_oct.R
@@ -1,7 +1,7 @@
 source('config/init.R')
 
 rs <- 
-  read_csv('Data/rideandseek2017_all_20171025_113704.csv') %>% 
+  read_csv('Data/rideandseek2017_all_20171103_122813.csv') %>% 
   select(phone_number) %>% 
   setNames('mobile') %>% 
   mutate(mobile = as.character(mobile))
@@ -9,10 +9,13 @@ rs <-
 meloct <-
   read_csv('Data/events_october.csv') %>% 
   setNames(c('event_id','mobile','northstar_id','timestamp')) %>% 
-  filter(timestamp <= '2017-10-25') 
+  filter(timestamp <= '2017-10-31') 
 
-countOct <- length(unique(na$northstar_id))
+countOct <- length(unique(meloct$northstar_id))
 
 rs %>% 
-  filter(!cleanPhone(mobile) %in% cleanPhone(meloct$mobile) & !duplicated(mobile))%>% 
-  nrow()
+  filter(
+    !cleanPhone(mobile) %in% cleanPhone(meloct$mobile) & 
+      !duplicated(mobile)
+    )%>% 
+  nrow() -> rssignups
