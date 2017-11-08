ef7873b630e65dbedffa96bf6af2a4e94f005ed3
shasan <sohaib3@icloud.com>
2017-11-08 17:33:41 -0500

    debugging

--- Infrastructure/Scripts/MEL_timestamp_debugging.sql
+++ Infrastructure/Scripts/MEL_timestamp_debugging.sql
@@ -12,9 +12,12 @@
 			  ORDER BY u.northstar_created_at_timestamp DESC, u.northstar_id, u.last_logged_in) a
  WHERE a.timestamp <= '1970-01-01 00:00:01' AND a.timestamp IS NOT NULL 
  LIMIT 5000;
+ #How many of these are niche
+ #How many people text in without signing up for a campaign?
+ #How many people register and have nothing else?
  
- SELECT * FROM quasar.users_log l WHERE l.northstar_id='59827155a0bfad7f2a09e76e' ORDER BY l.last_logged_in;
- SELECT * FROM quasar.users u WHERE u.northstar_id='59827155a0bfad7f2a09e76e';
+ SELECT * FROM quasar.users_log l WHERE l.northstar_id='59826fd8a0bfad01f90c7f71' ORDER BY l.last_logged_in;
+ SELECT * FROM quasar.users u WHERE u.northstar_id='59826fd8a0bfad01f90c7f71';
 
  SELECT a.*
  FROM 
