9e5d0ce452daeb33c7a222375214e2f39045ab98
shasan <sohaib3@icloud.com>
2018-02-20 18:14:34 -0500

    odds/ends

--- AdHoc/config/pgConnect.R
+++ AdHoc/config/pgConnect.R
@@ -6,15 +6,16 @@ pgConnect <- function() {
   user=Sys.getenv('QUASAR_USERNAME')
   pw=Sys.getenv('QUASAR_PG_PW')
   
-  channel <- 
+  channel <-
     dbConnect(
-      drv, 
-      user = "shasan", 
-      password=pw, 
-      dbname = "quasar", 
-      host = "quasar-pg.c9ajz690mens.us-east-1.rds.amazonaws.com", 
-      port = 5432
-      )
+      drv,
+      user = "shasan",
+      password=pw,
+      dbname = 'dbname=d4e7vm204dkmar sslmode=require',
+      host = "ec2-35-169-120-240.compute-1.amazonaws.com",
+      port = 5432#,
+      # sslmode="required"
+    )
   
   return(channel)
   
