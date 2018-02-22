5e0ac3f0c6c5ce3af0b213e928a2147f0b11785d
shasan <sohaib3@icloud.com>
2018-02-22 18:19:30 -0500

    get q1 sms nps

--- AdHoc/Scripts/typeformToRSample.R
+++ AdHoc/Scripts/typeformToRSample.R
@@ -13,11 +13,13 @@ allTypeForms <- fromJSON(json)
 
 # Grab the ID for the DoSomething Feedback survey
 allTypeForms %>% 
-  filter(name == 'DoSomething Feedback') %>% 
+  filter(name == 'DoSomething SMS Survey') %>% 
   select(id) %>% as.character() -> feedback
 
+smsq1Key <- 'ENmagA'
+
 # Submit request for that survey
-npsfeedback <- paste0('https://api.typeform.com/v1/form/',feedback,'?key=',key)
+npsfeedback <- paste0('https://api.typeform.com/v1/form/',smsq1Key,'?key=',key)
 res <- GET(url = npsfeedback)
 json <- content(res, as = "text")
 feedbackResults <- fromJSON(json)
@@ -25,3 +27,8 @@ feedbackResults <- fromJSON(json)
 # Grab questions and answers (including hidden fields) from request
 questions <- as.tibble(feedbackResults$questions)
 answers <- as.tibble(cbind(feedbackResults$responses$hidden, feedbackResults$responses$answers))
+
+answers %<>%
+  filter(!is.na(id))
+
+getNPS(answers$opinionscale_y5vvCZBD43lH,11)
