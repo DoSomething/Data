cf47c8fe410f50e7e9a4bb774e4cc315aaa83f88
shasan <sohaib3@icloud.com>
2018-03-07 16:09:07 -0500

    finish data prep

--- AdHoc/Scripts/VoterReg/turbovoteFile.R
+++ AdHoc/Scripts/VoterReg/turbovoteFile.R
@@ -1,32 +1,33 @@
 source('config/init.R')
 source('config/mySQLConfig.R')
+source('config/pgConnect.R')
 
 # Data prep ---------------------------------------------------------------
-
 getData <- function(path) {
   vr <- 
     read_csv(path) %>% 
     filter(
       !grepl('thing.org', email) & 
-        !grepl('testing', hostname) 
+        !grepl('testing', hostname) &
+        !grepl('@dosom', email)
     ) 
   
   for (i in 1:length(names(vr))) {
     if(grepl('-', names(vr)[i])) {
       names(vr)[i] <- gsub('-','_',names(vr)[i])
+    } else if(grepl(' ', names(vr)[i])) {
+      names(vr)[i] <- gsub(' ','_',names(vr)[i])
     }
   }
   return(vr)
 }
-
-vr <- getData('Data/testing-dosomething.turbovote.org-2018-03-06.csv')
 
 processReferralColumn <- function(dat) {
   maxSep <- max(as.numeric(names(table(str_count(dat$referral_code, ',')))))+1
   parsedSep <- 
     dat %>% 
     select(id, referral_code) %>% 
-    separate(referral_code, LETTERS[1:maxSep], ',', remove = F) %>% 
+    separate(referral_code, LETTERS[1:maxSep], ',') %>% 
     mutate(
       nsid = 
         case_when(
@@ -89,4 +90,70 @@ processReferralColumn <- function(dat) {
   return(parsedSep)
 }
 
-refParsed <- processReferralColumn(vr)
+getQuasarAttributes <- function(queryObjects) {
+  q <- paste0(
+    "SELECT
+      u.northstar_id AS nsid,
+      u.created_at AS ds_registration_date,
+      u.source AS user_source,
+      CASE WHEN u.customer_io_subscription_status = 'subscribed' OR 
+        u.sms_status = 'active' THEN 1 ELSE 0 END AS active_member,
+      c.signup_id, 
+      c.campaign_run_id,
+      max(CASE WHEN c.post_id <> -1 THEN 1 ELSE 0 END) as reportedback
+    FROM quasar.users u
+    LEFT JOIN quasar.campaign_activity c ON c.northstar_id = u.northstar_id
+    WHERE u.northstar_id IN ",queryObjects,"
+    GROUP BY u.northstar_id, c.signup_id
+    "
+  )
+  
+  nsrDat <- 
+    runQuery(q, 'mysql') %>% 
+    group_by(nsid) %>% 
+    summarise(
+      ds_registration_date = max(ds_registration_date),
+      user_source = max(user_source),
+      signups = n(),
+      reportbacks = sum(reportedback)
+    ) %>% 
+    mutate(
+      user_source = 
+        case_when(
+          user_source == 'niche' ~ 'niche',
+          user_source == 'sms' ~ 'sms',
+          TRUE ~ 'web'
+        )
+    )
+}
+
+prepData <- function(...) {
+  d <- getData(...)
+  refParsed <- processReferralColumn(d)
+  
+  vr <- 
+    d %>% 
+    left_join(refParsed) %>% 
+    group_by(nsid) %>% 
+    filter(updated_at == max(updated_at) | nsid=='') %>% 
+    ungroup()
+  
+  nsids <- 
+    vr %>% 
+    filter(nsid != '') %$%
+    nsid %>% 
+    prepQueryObjects()
+  
+  nsrDat <- getQuasarAttributes(nsids)
+  
+  vr %<>%
+    left_join(nsrDat)
+  
+  return(vr)
+}
+
+vr <- prepData(path='Data/testing-dosomething.turbovote.org-2018-03-06.csv')
+
+# Analysis ----------------------------------------------------------------
+
+
