1ebca82be9045b9e0d9a109d815e0ddfa1004d9e
shasan <sohaib3@icloud.com>
2018-03-19 17:45:25 -0400

    cleanup

--- AdHoc/Scripts/m4ol_responses.js
+++ AdHoc/Scripts/m4ol_responses.js
@@ -1,7 +1,8 @@
 /*https://trello.com/c/G0rMrSRR/1248-data-request-sms-week-of-action-march-for-our-lives-rivescript-query*/
 records = [];
 var cursor = db.getCollection('conversations').find({
-    topic: { $in: [ "m4ol_attending", "m4ol_supporting", "m4ol_moreinfo", "m4ol_declined"] }
+    direction: 'inbound', 
+    topic: { $in: [ "m4ol_attending", "m4ol_supporting", "m4ol_moreinfo", "m4ol_declined"] },
     },
     {_id: 0, userId: 1, topic: 1});
 while(cursor.hasNext()) {
