ff3bed7a62e19db701f0cee3320332786b80ccbd
shasan <sohaib3@icloud.com>
2018-04-06 10:17:53 -0400

    cleanuo and next steps

--- SPIS/Scripts/analyzeQuestionTypes.R
+++ SPIS/Scripts/analyzeQuestionTypes.R
@@ -3,33 +3,33 @@ library(rlang)
 options(useFancyQuotes = F)
 
 buildMapping <- function(replace, inCode, outCode) {
+
   mapping <- exprs()
 
   for (i in 1:length(inCode)) {
+
     mapping[[i]] <-
       bquote(get(.(replace)) == .(inCode[i]) ~ .(outCode[i]))
-  }
-  return(mapping)
-}
 
-mapFrom <- c('Strongly Disagree','2','3','4','Strongly Agree')
-mapTo <- c('Strongly Disagree','Disagree','Neutral','Agree','Strongly Agree')
-finCode <- c(-2,-1,0,1,2)
+  }
+
+  return(mapping)
+
+}
 
 lookupMaker <- function(input, mapFrom, mapTo, finCode) {
 
   input <- enquo(input)
   input.s <- paste0(input)[2]
-  var_name <- quo_name(input)
 
-  mapping <- buildMapping(input.s, mapFrom, mapTo)
+  recodeMapping <- buildMapping(input.s, mapFrom, mapTo)
   outcomeMapping <- buildMapping(input.s, mapFrom, finCode)
 
   lookupTable <-
     set %>%
     count(!!input) %>%
     mutate(
-      recoded = case_when(!!!mapping),
+      recoded = case_when(!!!recodeMapping),
       outcome = case_when(!!!outcomeMapping)
     ) %>%
     select(-n)
@@ -37,11 +37,6 @@ lookupMaker <- function(input, mapFrom, mapTo, finCode) {
   return(lookupTable)
 
 }
-
-lookupMaker(
-  impact_attitudes.I_make_an_active_effort_to_understand_others_perspectives,
-  mapFrom = mapFrom, mapTo = mapTo, finCode=finCode
-  )
 
 analyzeStyleRank <- function(outcome, pivots, ...) {
   browser()
@@ -56,7 +51,16 @@ analyzeStyleRank <- function(outcome, pivots, ...) {
     select(!!outcome, !!pivots) %>%
     left_join(outcomeLookup)
 
+  ## Insert decision tree to select pivots
+  ## Generate overall frequency overall average value
+  ## Generate pivoted frequencies and avg values
+  ## Plot all of the above
+
 }
+
+mapFrom <- c('Strongly Disagree','2','3','4','Strongly Agree')
+mapTo <- c('Strongly Disagree','Disagree','Neutral','Agree','Strongly Agree')
+finCode <- c(-2,-1,0,1,2)
 
 analyzeStyleRank(
   impact_attitudes.I_make_an_active_effort_to_understand_others_perspectives,
