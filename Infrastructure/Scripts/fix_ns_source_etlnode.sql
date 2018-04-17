227aea8cccf4ee4d2ccd9a34d573f45f0765a70f
shasan <sohaib3@icloud.com>
2018-04-17 13:25:09 -0400

    tweak source

--- Infrastructure/Scripts/fix_ns_source_etlnode.sql
+++ Infrastructure/Scripts/fix_ns_source_etlnode.sql
@@ -1,8 +1,7 @@
 SELECT 
 	u.northstar_id,
 	u.email,
-	u.source,
-	COALESCE(m.email, i.email) AS email,
+	'mailchimp' AS source,
 	COALESCE(m.confirm_time, i.confirm_time) AS created_at
 FROM quasar.users u 
 LEFT JOIN mailchimp_final_exports.final_mailchimp_main_sub m ON m.email = u.email
