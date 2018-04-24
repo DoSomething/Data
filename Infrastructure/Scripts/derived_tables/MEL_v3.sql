303676a57bea7bcedfac08cf19462e308c80f513
James Jensen <jjensen@dosomething.org>
2018-04-24 13:22:42 -0400

    added voter reg and shares to MEL

--- Infrastructure/Scripts/derived_tables/MEL_v3.sql
+++ Infrastructure/Scripts/derived_tables/MEL_v3.sql
@@ -4,7 +4,8 @@ SELECT
     a.timestamp AS timestamp,
     a.action AS action_type,
     a.action_id AS action_id,
-   	a.source AS source
+   	a.source AS source,
+   	a.action_serial_id AS action_serial_id
 FROM ( 
 	SELECT -- CAMPAIGN SIGNUP
 		DISTINCT s.northstar_id AS northstar_id,
@@ -79,7 +80,33 @@ FROM (
 		cio.email_event cio
 	WHERE 
 		cio.event_type = 'email_clicked'
+	UNION ALL
+	SELECT -- VOTER REGISTRATIONS
+		DISTINCT tv.nsid AS northstar_id,
+		tv.created_at AS timestamp,
+		'registered' AS action,
+		'8' AS action_id,
+		tv.source_details AS source,
+		tv.id as action_serial_id
+	FROM
+		public.turbovote_file tv
+	WHERE
+		tv.ds_vr_status IN ('register-form', 'confirmed', 'register-OVR')
+	AND 
+		tv.nsid IS NOT NULL AND tv.nsid <> ''
+	UNION ALL 
+	SELECT -- FACEBOOK SHARES FROM PHOENIX-NEXT
+		DISTINCT pe.northstarid_s AS northstar_id,
+		to_timestamp(pe.ts /1000) AS timestamp,
+		'facebook share completed' AS action,
+		'9' AS action_id,
+		pe.event_source AS source,
+		pe.event_id AS action_serial_id
+	FROM 
+		public.phoenix_next_events pe
+	WHERE 
+		pe.event_name IN ('share action completed', 'facebook share posted')
+	AND pe.northstarid_s IS NOT NULL
+	AND pe.northstarid_s <> ''
 	) AS a 
-WHERE a.action = 'clicked_link'
-ORDER BY timestamp DESC 
 LIMIT 1000
