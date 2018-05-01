bb1191e41ebe57465980233183c626685e4b10f4
shasan <sohaib3@icloud.com>
2018-05-01 19:55:48 -0400

    Code is ready, needs interpretation

--- KPIs/Scripts/MAMReport/MAMReport.R
+++ KPIs/Scripts/MAMReport/MAMReport.R
@@ -2,34 +2,34 @@ source('config/init.R')
 library(scales)
 
 mm <-
-  runQuery('Scripts/MAMReport_usertypes.sql', 'pg')
+  runQuery('Scripts/MAMReport/usertypes.sql', 'pg') %>%
+  filter(Month > '2017-01-01' & Month < '2018-05-01')
 
 qres <-
-  runQuery('Scripts/MAMReport_actiontypes.sql', 'pg')
+  runQuery('Scripts/MAMReport/actiontypes.sql', 'pg') %>%
+  filter(Month > '2017-01-01' & Month < '2018-05-01')
 
 act <-
   qres %>%
   group_by(Month, niche) %>%
   mutate(
     prop = actions/sum(actions),
-    Text = percent(prop),
-    position = 1-cumsum(prop) + (.5*prop)
+    Text = percent(prop)
   )
 
 act.N <-
   qres %>%
-  group_by(month, action_type) %>%
+  group_by(Month, action_type) %>%
   summarise(
-    count = sum(count)
+    count = sum(actions)
   ) %>%
   mutate(
     prop = count/sum(count),
-    Text = percent(prop),
-    position = 1-cumsum(prop) + (.5*prop)
+    Text = percent(prop)
   )
 
 da <-
-  runQuery('Scripts/MAMReport_dailyactive.sql', 'pg') %>%
+  runQuery('Scripts/MAMReport/dailyactive.sql', 'pg') %>%
   mutate(
     DayOfWeek = factor(weekdays(date),
                        levels=c('Monday','Tuesday','Wednesday','Thursday',
--- KPIs/Scripts/MAMReport/dailyactive.sql
+++ KPIs/Scripts/MAMReport/dailyactive.sql
@@ -1,6 +1,7 @@
 SELECT 
-	date_trunc('day', m."timestamp") AS "date",
-	count(DISTINCT m.northstar_id) AS actions
+	date_trunc('day', m."timestamp")::date AS "date",
+	count(DISTINCT m.northstar_id) AS active_members
 FROM member_event_log m
 WHERE m."timestamp" >= '2017-01-01'
 GROUP BY date_trunc('day', m."timestamp")
+;
