c513319036bd82bbaf4c87205ec647b83fac09fc
shasan <sohaib3@icloud.com>
2018-05-18 10:59:33 -0400

    md5 for event id and added as primary key

--- Infrastructure/Scripts/cio_status_backfill_testing.sql
+++ Infrastructure/Scripts/cio_status_backfill_testing.sql
@@ -5,7 +5,7 @@ CREATE TABLE public.legacy_cio_status_import AS
 		u.northstar_id AS customer_id, 
 		u.email AS email_address, 
 		NULL AS template_id,
-		NULL AS event_id,
+		MD5(CONCAT(u.northstar_id, u.email, u.customer_io_subscription_timestamp)) AS event_id,
 		u.customer_io_subscription_timestamp AS "timestamp", 
 		CASE 
 			WHEN u.customer_io_subscription_status = 'subscribed' THEN 'customer_subscribed'
@@ -16,5 +16,5 @@ CREATE TABLE public.legacy_cio_status_import AS
 	)
 ;
 
-ALTER TABLE public.legacy_cio_status_import ADD PRIMARY KEY (customer_id, "timestamp", event_type);
-CREATE INDEX leg_cio_indices ON public.legacy_cio_status_import (customer_id, "timestamp", event_type);
+ALTER TABLE public.legacy_cio_status_import ADD PRIMARY KEY (event_id, customer_id, "timestamp", event_type);
+CREATE INDEX leg_cio_indices ON public.legacy_cio_status_import (event_id, customer_id, "timestamp", event_type);
