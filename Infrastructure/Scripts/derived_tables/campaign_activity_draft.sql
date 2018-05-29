ae29f4abdad01481afba742a16704f79d6eff19c
shasan <sohaib3@icloud.com>
2018-05-29 18:32:38 -0400

    draft of revised campaign activity with "reportback_volume" and "post_class"

---
+++ Infrastructure/Scripts/derived_tables/campaign_activity_draft.sql
@@ -0,0 +1,124 @@
+DROP TABLE IF EXISTS signups_test ;
+CREATE TEMPORARY TABLE signups_test AS 
+    (SELECT 
+        sd.northstar_id AS northstar_id,
+        sd.id AS id,
+        sd.campaign_id AS campaign_id,
+        sd.campaign_run_id AS campaign_run_id,
+        sd.why_participated AS why_participated,
+        sd."source" AS "source",
+        sd.created_at AS created_at
+    FROM 
+        (SELECT 
+        		stemp.id,
+        		max(stemp.updated_at) AS updated_at
+        FROM rogue.signups stemp
+        WHERE stemp.deleted_at IS NULL
+        AND stemp."source" IS DISTINCT FROM 'runscope'
+        AND stemp."source" IS DISTINCT FROM 'runscope-oauth'
+        AND stemp.why_participated IS DISTINCT FROM 'why_participated_ghost_test'
+        GROUP BY stemp.id) s_maxupt
+        INNER JOIN rogue.signups sd
+            ON sd.id = s_maxupt.id AND sd.updated_at = s_maxupt.updated_at
+    )
+    ; 
+CREATE INDEX signups_testi ON signups_test (id, created_at); 
+
+DROP TABLE IF EXISTS latest_post_test CASCADE;
+CREATE TEMPORARY TABLE latest_post_test AS
+    (SELECT 
+        pd.id AS id,
+        pd."type" AS "type",
+        pd."action" AS "action",
+        pd.status AS status,
+        pd.quantity AS quantity,
+        pd."source" AS "source",
+        pd.created_at AS created_at,
+        pd.url AS url,
+        pd.signup_id AS signup_id
+    FROM 
+        (SELECT 
+            ptemp.id,
+            max(ptemp.updated_at) AS updated_at
+         FROM rogue.posts ptemp
+         WHERE ptemp.deleted_at IS NULL
+         AND ptemp."source" IS DISTINCT FROM 'runscope'
+         AND ptemp."source" IS DISTINCT FROM 'runscope-oauth'
+        GROUP BY ptemp.id) p_maxupt
+     INNER JOIN rogue.posts pd
+            ON pd.id = p_maxupt.id AND pd.updated_at = p_maxupt.updated_at  
+    )
+    ;
+CREATE INDEX latest_post_testi ON latest_post_test (id, created_at); 
+
+DROP TABLE IF EXISTS posts_test CASCADE;
+CREATE TEMPORARY TABLE posts_test AS 
+    (SELECT 
+            pd.id AS id,
+            pd."type" AS "type",
+            pd."action" AS "action",
+            pd.status AS status,
+            pd.quantity AS quantity,
+            pd."source" AS "source",
+            COALESCE(tv.created_at, pd.created_at) AS created_at,
+            pd.url AS url,
+            pd.signup_id AS signup_id
+    FROM latest_post_test pd
+    LEFT JOIN rogue.turbovote tv ON tv.post_id::bigint = pd.id)
+;
+CREATE INDEX post_testi ON posts_test (id, created_at); 
+
+DROP TABLE IF EXISTS reported_back_test CASCADE;
+CREATE TEMPORARY TABLE reported_back_test AS 
+    (SELECT 
+        temp_posts.signup_id,
+        MAX(CASE WHEN temp_posts.id <> -1 THEN 1 ELSE 0 END) AS reported_back
+    FROM 
+        latest_post_test temp_posts
+    WHERE temp_posts.signup_id IS NOT NULL
+    GROUP BY 
+        temp_posts.signup_id
+    ) 
+    ; 
+CREATE INDEX reported_back_testi ON reported_back_test (signup_id);
+
+DROP TABLE IF EXISTS ca_test;
+CREATE TEMPORARY TABLE ca_test AS 
+    (SELECT  
+        a.northstar_id AS northstar_id,
+        a.id AS signup_id,
+        b.id AS post_id,
+        a.campaign_id AS campaign_id,
+        a.campaign_run_id AS campaign_run_id,
+        b."type" AS post_type,
+        b."action" AS post_action,
+        CASE WHEN a.campaign_id = '822' AND a.created_at >= '2018-05-01' 
+        		 THEN 'voter-reg - ground'
+        		 ELSE CONCAT(b."type", ' - ', b."action") END AS post_class,
+        	CASE WHEN b.id IS NULL THEN NULL
+        		 WHEN a.campaign_id = '822' AND a.created_at >= '2018-05-01' AND b.status = 'accepted' THEN b.quantity
+        		 ELSE 1 END AS reportback_volume,
+        b.status AS post_status,
+        a.why_participated AS why_participated,
+        b.quantity AS quantity,
+        a."source" AS signup_source,
+        b."source" AS post_source,
+        a.created_at AS signup_created_at,
+        b.created_at AS post_created_at,
+        c.reported_back AS reported_back,
+        b.url AS url
+    FROM 
+        signups_test a
+    LEFT JOIN 
+        posts_test b
+        ON b.signup_id = a.id
+    LEFT JOIN 
+        reported_back_test c
+        ON c.signup_id = a.id
+    )
+    ;
+CREATE INDEX ON public.campaign_activity (northstar_id, signup_id, post_id, post_created_at);
+GRANT SELECT ON public.campaign_activity TO looker;
+GRANT SELECT ON public.campaign_activity TO jjensen;
+GRANT SELECT ON public.campaign_activity TO jli;
+GRANT SELECT ON public.campaign_activity TO shasan;
