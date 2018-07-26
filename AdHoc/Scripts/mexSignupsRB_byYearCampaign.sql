0dd1d55ad090628182c91cf7b1f4691786bf192a
shasan <sohaib3@icloud.com>
2018-07-26 11:14:31 -0400

    switch to postgres

--- AdHoc/Scripts/mexSignupsRB_byYearCampaign.sql
+++ AdHoc/Scripts/mexSignupsRB_byYearCampaign.sql
@@ -1,6 +1,6 @@
-#https://trello.com/c/rczVQ2DE/1158-mexico-quarterly-updates
+--https://trello.com/c/rczVQ2DE/1158-mexico-quarterly-updates
 SELECT 
-	ca.`year`,
+	ca."year",
 	ca.campaign_run_id,
 	i.campaign_node_id_title,
 	count(*) AS signups,
@@ -9,33 +9,28 @@ FROM
 	(SELECT 
 		c.signup_id,
 		c.campaign_run_id,
-		year(c.signup_created_at) AS 'year',
+		EXTRACT('year' FROM c.signup_created_at) AS "year",
 		max(CASE WHEN c.post_id <> -1 THEN 1 ELSE 0 END) AS reportbacks
-	FROM quasar.users u
-	LEFT JOIN quasar.campaign_activity c ON u.northstar_id=c.northstar_id
+	FROM public.users u
+	LEFT JOIN public.campaign_activity c ON u.northstar_id=c.northstar_id
 	WHERE u.country='MX'
 	AND c.signup_created_at >= '2016-01-01'
-	GROUP BY c.signup_id) ca
-LEFT JOIN quasar.campaign_info i ON ca.campaign_run_id = i.campaign_run_id
-GROUP BY ca.`year`, ca.campaign_run_id
+	GROUP BY 1,2,3) ca
+LEFT JOIN public.campaign_info i ON ca.campaign_run_id = i.campaign_run_id
+GROUP BY 1,2,3
 ;
 
-SELECT DISTINCT 
-	ca.post_id,
-	ca.submission_created_at,
-	ca.url
-FROM 
-	(SELECT 
-		c.signup_id,
-		c.post_id,
-		c.submission_created_at,
-		c.url,
-		max(c.submission_updated_at) AS max_submission
-	FROM quasar.users u
-	INNER JOIN quasar.campaign_activity c ON u.northstar_id=c.northstar_id
-	WHERE u.country='MX'
-	AND c.post_id <> -1 
-	AND c.signup_created_at >= '2016-01-01'
-	GROUP BY c.signup_id) ca
-INNER JOIN quasar.campaign_activity camp ON camp.signup_id = ca.signup_id AND camp.submission_updated_at = ca.max_submission
+SELECT count(*) AS count_active_users FROM public.users u WHERE u.subscribed_member = TRUE AND u.country='MX' 
+
+;
+SELECT 
+	c.signup_id,
+	c.post_id,
+	c.post_created_at,
+	c.url
+FROM public.users u
+INNER JOIN public.campaign_activity c ON u.northstar_id=c.northstar_id
+WHERE u.country='MX'
+AND c.post_id <> -1 
+AND c.signup_created_at >= '2016-01-01'
 ;
