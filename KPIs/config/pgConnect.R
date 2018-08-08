2aa876ea86c35584636bd7677e44638a02471d7d
shasan <sohaib3@icloud.com>
2018-08-08 11:05:26 -0400

    qa prep

--- KPIs/config/pgConnect.R
+++ KPIs/config/pgConnect.R
@@ -1,18 +1,27 @@
 library(RPostgreSQL)
 
-pgConnect <- function() {
+pgConnect <- function(QA=F) {
 
   drv <- dbDriver("PostgreSQL")
   user=Sys.getenv('QUASAR_USERNAME')
-  pw=Sys.getenv('QUASAR_PG_PW')
+
+  if (QA==T) {
+    pw=Sys.getenv('QUASAR_QA_PW')
+    host="ec2-18-211-176-65.compute-1.amazonaws.com"
+    db="deh9oi912c63mc"
+  } else {
+    pw=Sys.getenv('QUASAR_PG_PW')
+    host="ec2-52-201-106-141.compute-1.amazonaws.com"
+    db="d4e7vm204dkmar"
+  }
 
   channel <-
     dbConnect(
       drv,
       user = user,
       password=pw,
-      dbname = 'dbname=d4e7vm204dkmar sslmode=require',
-      host = "ec2-52-201-106-141.compute-1.amazonaws.com",
+      dbname = paste0('dbname=',db,' sslmode=require'),
+      host = host,
       port = 5432#,
       # sslmode="required"
     )
