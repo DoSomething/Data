dbd575cdd7ccca83fc3306d3c8d52df40a3f6036
shasan <sohaib3@icloud.com>
2018-09-06 10:39:02 -0400

    remove bad emails

--- KPIs/Scripts/VoterReg/rtvFileChunkChompy.R
+++ KPIs/Scripts/VoterReg/rtvFileChunkChompy.R
@@ -1,4 +1,27 @@
-rawRTV <- read_csv('Data/RockTheVote/rock_the_vote_2018-08-29.csv')
+badEmails <-
+  c(
+    'testing123@email.com',
+    'testing1234@email.com',
+    'testing12349@email.com',
+    'peanuts123@gmail.com',
+    'kitty666@gmail.com',
+    'testing123@gmail.com',
+    'Shay@gmail.com',
+    'mariusandeponime@gmail.com',
+    'spark@dosomething.org',
+    'puppetsloth111@email.com',
+    'lkpttn@gmail.com'
+  )
+
+rawRTV <-
+  read_csv('Data/RockTheVote/rock_the_vote_2018-09-04.csv') %>%
+  filter(
+    `Email address` != badEmails
+  ) %>%
+  mutate(
+    `Tracking Source` = gsub('user\\:\\{userid\\}\\,','',`Tracking Source`)
+  )
+
 
 allSets <- split(rawRTV, as.numeric(rownames(rawRTV)) %/% 1200)
 
