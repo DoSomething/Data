4335dae076cc6fd62581f7152ba2ca0b56f3d0ec
shasan <sohaib3@icloud.com>
2018-09-14 12:52:27 -0400

    more plots!

--- SPIS/Scripts/makePresentationPlots.R
+++ SPIS/Scripts/makePresentationPlots.R
@@ -65,6 +65,77 @@ politicsAge <-
   guides(fill=guide_legend(title="Political View"))
 ggsave(plot=politicsAge, 'Visuals/politicsAge.png', width = 8, height = 6)
 
+genderRaceGroup <-
+  stylePickOneList(set, sex, race, Group)$Facetted +
+  labs(title='Gender & Race Comparison') +
+  guides(fill=guide_legend(title="Race"))
+ggsave(plot=genderRaceGroup, 'Visuals/genderRaceGroup.png', width = 10, height = 6)
+
+
+# Brand -------------------------------------------------------------------
+
+purchaseDecision <-
+  bind_rows(
+    tibble(
+      Feature='Price',
+      Value=weighted.mean(purchaseInfluence.Price$frequencyPlot$data$outcome,
+                          purchaseInfluence.Price$frequencyPlot$data$count)
+    ),
+    tibble(
+      Feature='Quality',
+      Value=weighted.mean(purchaseInfluence.Quality$frequencyPlot$data$outcome,
+                          purchaseInfluence.Quality$frequencyPlot$data$count)
+    ),
+    tibble(
+      Feature='Celebrity Endorsement',
+      Value=weighted.mean(purchaseInfluence.Celebrity$frequencyPlot$data$outcome,
+                          purchaseInfluence.Celebrity$frequencyPlot$data$count)
+    ),
+    tibble(
+      Feature='Company Values',
+      Value=weighted.mean(purchaseInfluence.CompanyReputationValues$frequencyPlot$data$outcome,
+                          purchaseInfluence.CompanyReputationValues$frequencyPlot$data$count)
+    ),
+    tibble(
+      Feature='Customer Reviews',
+      Value=weighted.mean(purchaseInfluence.CustomerReviews$frequencyPlot$data$outcome,
+                          purchaseInfluence.CustomerReviews$frequencyPlot$data$count)
+    ),
+    tibble(
+      Feature='Packaging',
+      Value=weighted.mean(purchaseInfluence.Packaging$frequencyPlot$data$outcome,
+                          purchaseInfluence.Packaging$frequencyPlot$data$count)
+    ),
+    tibble(
+      Feature='Friend/Family Recommended',
+      Value=weighted.mean(purchaseInfluence.RecommendedByKnownPerson$frequencyPlot$data$outcome,
+                          purchaseInfluence.RecommendedByKnownPerson$frequencyPlot$data$count)
+    )
+  )
+
+grid.arrange(
+  purchaseInfluence.Celebrity$frequencyPlot,
+  purchaseInfluence.CompanyReputationValues$frequencyPlot,
+  purchaseInfluence.CustomerReviews$frequencyPlot,
+  purchaseInfluence.Packaging$frequencyPlot,
+  purchaseInfluence.Price$frequencyPlot,
+  purchaseInfluence.Quality$frequencyPlot,
+  purchaseInfluence.RecommendedByKnownPerson$frequencyPlot,
+  ncol=3
+)
+
+purchaseDecision.p <-
+  ggplot(purchaseDecision, aes(x=reorder(Feature, -Value), y=Value)) +
+  geom_bar(stat='identity', fill='#6ac6b4') +
+  labs(x='Influencer', y='How Influential',
+       title='How Influential is Each of These Items When Purchasing a Product',
+       caption=
+         "Influence is the average value where -2 is 'Not at all Influential', 0 is 'Neutral', and 2 is 'Very Influential'") +
+  theme(plot.title = element_text(hjust=.5))
+ggsave(plot=purchaseDecision.p, 'Visuals/purchaseDecision.png', width = 10, height = 6)
+
+purchaseInfluence.Price$pivotPlot[[1]]
+
 # Social Action Types by Age ----------------------------------------------
 
 p1 <-
