dd216d58d518db79ba9034d96197d55375c62a88
Megha Jain <mjain@Meghas-Work-MacBook-Pro.local>
2018-11-05 11:00:26 -0500

    supporting utils file for notebook

---
+++ KPIs/Scripts/MelAudit/utils.py
@@ -0,0 +1,47 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import pandas as pd
+import psycopg2
+import seaborn as sns
+from collections import OrderedDict
+
+
+def print_null_vals(df):
+    null_vals = []
+    for col in df.columns:
+        if df[col].dtype == 'object': 
+            null_val = sum(df[col].isnull()) + sum(df[col] == '') + sum(df[col] == '0')
+        if df[col].dtype == 'datetime64[ns]':
+            null_val = sum(df[col].isnull()) + sum(df[col] == '')
+        if df[col].dtype == 'float64' or df[col].dtype == 'int64':
+            null_val = sum(df[col].isnull())
+        null_vals.append(null_val)
+
+    df_nulls = pd.DataFrame({
+        'col': df.columns,
+        'num_null': null_vals,
+        'percent_null': list(np.array(null_vals) / len(df))
+    })
+
+    return df_nulls
+
+
+def load_connection():
+    user = os.environ.get('DB_USER')
+    pwd = os.environ.get('DB_PW')
+    db = os.environ.get('DB_NAME')
+    host = os.environ.get('DB_HOST')
+    return psycopg2.connect(database=db, user=user, password=pwd, host=host)
+
+
+def analyze_df_group(df):
+    df = df.sort_values(['identifier', 'click_time'])
+    df_first = df[~(
+        df.duplicated(['northstar_id', 'broadcast_id'], keep='first'))]
+    df_first['time_to_click'] = df_first['click_time'] - df_first['created_at']
+    df_first['time_to_click'] = df_first['time_to_click'].apply(
+        lambda x:  x.total_seconds())
+    df_first = df_first[~(df_first['time_to_click'] < 0)]
+
+    return df_first
