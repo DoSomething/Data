f24faca4186e7b006914fbdde1e071741b18d35e
jli@dosomething.org <jli@dosomething.org>
2018-12-18 16:16:09 -0500

    Fixed scripts. Save_NPS_toQuasar cleans fields first.

--- Sentiment/Transacational_NPS/Save_NPS_toQuasar.R
+++ Sentiment/Transacational_NPS/Save_NPS_toQuasar.R
@@ -1,15 +1,49 @@
 source('config/pgConnect.R')
 channel <- pgConnect()
 
-#Create transactional NPS table
-nps_data <- 
-  tibble(
-    collected_at = as.Date(c('2018-08-30','2018-09-07','2018-09-30','2018-10-24','2018-10-24','2018-10-31')),
-    channel = c('sms','email','web','sms','email','web'),
-    score = c(27,-6, 32, 10,11,37)
-  )
+#Upload Typeform csvs to PostGres
+nps_sms <- read.csv('~/Downloads/DoSomething SMS Q4 (1).csv')%>%
+  rename(nps=starts_with("Considering"),
+         nps_reason=starts_with("You"),
+         start_date =starts_with("Start"),
+         submit_date=starts_with("Submit"),
+         nsid=user_id)%>%
+  select(-X.,-Network.ID)
 
-dbWriteTable(channel, c("survey", "nps_transactional"), nps_data, row.names=F)
+nps_email <- read.csv('~/Downloads/DoSomething Email Q4 (3).csv')%>%
+  rename(nps=starts_with("Considering"),
+         nps_reason=starts_with("You"),
+         start_date =starts_with("Start"),
+         submit_date=starts_with("Submit"),
+         nsid=user_id)%>%
+  select(-X.,-Network.ID)
 
-grant <- "grant select on survey.nps_transactional to jli,shasan, mjain, ubrtjh45jniptr;"
+nps_web <- read.csv('~/Downloads/Would you take this quick and easy survey_ (9).csv')%>%
+  rename(nps=starts_with("Considering"),
+         nps_reason=starts_with("Any"),
+         start_date =starts_with("Start"),
+         submit_date=starts_with("Submit"),
+         nsid=northstar_id)%>%
+  select(-X.,-Network.ID, -campaign_id, -legacy_campaign_id,-origin)
+nps_web$source <- 'web'
+
+
+########################
+#Join all channel data#
+#######################
+nps_typeforms <- bind_rows(nps_email,nps_sms,nps_web)
+
+#add in NPS category, create channel var (each month, change to relevant month in source == statement)
+nps_typeforms<- nps_typeforms%>%
+  mutate(nps_cat = case_when(nps<7 ~ 'Detractor',
+                             nps %in% c(7,8) ~ 'Persuadable',
+                             nps>8 ~ 'Promoter'),
+         submit_date=as.Date(substr(submit_date, 1, 10), origin='1970-01-01'),
+         channel=
+           case_when(source %like% "email" ~ 'email',
+                     source %like% "sms" ~ 'sms',
+                     source=='web' ~ 'web'))
+
+dbWriteTable(channel, c("survey", "nps_typeforms"), nps_typeforms, row.names=F)
+grant <- "grant select on survey.nps_typeforms to jli,shasan, mjain, ubrtjh45jniptr;"
 dbGetQuery(channel, grant)
--- Sentiment/Transacational_NPS/nps_transactional_all.R
+++ Sentiment/Transacational_NPS/nps_transactional_all.R
@@ -1,57 +1,21 @@
 #Pull SMS, Email and Web NPS Typeforms
-nps_sms <- "select *
-        from survey.nps_sms"
-nps_sms <- runQuery(nps_sms)
-#rename nps and nsid
-nps_sms <- nps_sms%>%
-  rename(nps=starts_with("Considering"),
-         nsid=user_id)
-
-nps_email <- "select *
-from survey.nps_email"
-nps_email <- runQuery(nps_email)
-#rename nps and nsid
-nps_email <- nps_email%>%
-  rename(nps=starts_with("Considering"),
-         nsid=user_id)
-
-nps_web <- "select *
-from survey.nps_web"
-nps_web <- runQuery(nps_web)
-#rename nps and nsid
-nps_web <-nps_web%>%
-  rename(nps=starts_with("Considering"),
-         nsid=northstar_id)%>%
-  select(-campaign_id,-legacy_campaign_id,-origin)
-nps_web$source <- 'web'
-
-########################
-#Join all channel data#
-#######################
-nps_trans <- bind_rows(nps_email,nps_sms,nps_web)
-
-#add in NPS category, create channel var (each month, change to relevant month in source == statement)
-nps_trans<- nps_trans%>%
-  mutate(nps_cat = case_when(nps<7 ~ 'Detractor',
-                             nps %in% c(7,8) ~ 'Persuadable',
-                             nps>8 ~ 'Promoter'),
-         submit_date=as.Date(substr(Submit.Date..UTC., 1, 10), origin='1970-01-01'),
-         channel=
-           case_when(source=='emailDec' ~ 'email',
-                     source=='smsDec' ~ 'sms',
-                     source=='web' ~ 'web'))
+nps_trans <- "select *
+        from survey.nps_typeforms"
+nps_trans <- runQuery(nps_trans)
 
 ##########################
-####Get SMS NPS scores####
+####Get NPS scores####
 #########################
+
+#Each month change to relevant month in source== statement
 nps_sms_month <- nps_trans%>%
-  filter(channel=='sms' & !duplicated(nsid))
+  filter(channel=='sms' & !duplicated(nsid) & source =='smsDec')
 getNPS(nps_sms_month$nps,10)%>%
   print(nps_score)
 
-#Get Email NPS score
+#Each month change to relevant month in source== statement
 nps_email_month <- nps_trans%>%
-  filter(channel=='email' & !duplicated(nsid))
+  filter(channel=='email' & !duplicated(nsid) & source =='emailDec')
 getNPS(nps_email_month$nps,10)%>%
   print(nps_score)
 
@@ -77,7 +41,7 @@ nps_data <-
     score = c(27,-6, 32, 10,11,37,15,4,27,9,29,29)
   )
 
-dbWriteTable(channel, c("survey", "nps_transactional"), nps_data, row.names=F)
+dbWriteTable(channel, c("survey", "nps_transactional"), nps_data, row.names=F, append=T)
 
 grant <- "grant select on survey.nps_transactional to jli,shasan, mjain, ubrtjh45jniptr;"
 dbGetQuery(channel, grant)
