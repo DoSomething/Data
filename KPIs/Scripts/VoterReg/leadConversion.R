9437ff91d4bf51b3602992448c93d770ae115c36
Megha Jain <mjain@Meghas-Work-MacBook-Pro.local>
2019-01-30 10:23:46 -0500

    add analysis by age

--- KPIs/Scripts/VoterReg/leadConversion.R
+++ KPIs/Scripts/VoterReg/leadConversion.R
@@ -1,6 +1,7 @@
 library(stringr)
 library(openxlsx)
 library(dplyr)
+library(eeptools)
 source('Scripts/VoterReg/rogueTVRTV.R')
 
 library(reshape2)
@@ -65,3 +66,54 @@ grouped_df <-
   group_by(df, lead_source) %>%
   summarise(count_confirmed = sum(!is.na(ds_vr_status)), count_total = n()) %>%
   mutate(conversion = count_confirmed / count_total)
+
+
+# pull in age column to look at conversion by age group using RTV file
+rtvfile <- 'Data/rock_the_vote_2019-01-17.csv'
+
+rtv_extra <-
+  suppressWarnings(read_csv(rtvfile)) %>%
+  filter(Status == 'Complete') %>%
+  select(
+    email=`Email address`, dob=`Date of birth`
+  ) %>%
+  mutate(
+    dob = as.Date(dob, '%m/%d/%Y')
+  ) %>%
+  distinct(., email, .keep_all = TRUE)
+
+df <-
+  left_join(df, rtv_extra, by = c('Email' = 'email')) %>%
+  filter(!is.na(dob)) %>%
+  mutate(
+    created_at = as.Date(as.POSIXct(created_at, 'GMT'))
+  )
+
+df <-
+  filter(df, df$dob < df$created_at)
+
+df <-
+  mutate(
+    df,
+    age = floor(age_calc(df$dob, df$created_at, 'years'))
+  )
+
+df <-
+  mutate(
+    df,
+    age_group = cut(
+    df$age,
+    breaks=c(0, 13, 18, 25, 35, 45, 55, 65, Inf),
+    right = FALSE,
+    labels=c('< 13', '13-17', '18-24', '25-34', '35-44', '45-54', '55-64', '65+'))
+  )
+
+df_byage <-
+  df %>%
+  group_by(age_group) %>%
+  summarise(count = n())
+
+df_bychannel_byage <-
+  df %>%
+  group_by(lead_source, age_group) %>%
+  summarise(count = n())
