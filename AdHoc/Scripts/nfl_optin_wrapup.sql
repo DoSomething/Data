f12a8e3cee850e49174d371747defca20a3c7ccd
Megha Jain <mjain@Meghas-Work-MacBook-Pro.local>
2019-07-12 13:44:34 -0400

    updated script per their column/field value requirements

--- AdHoc/Scripts/nfl_optin_wrapup.sql
+++ AdHoc/Scripts/nfl_optin_wrapup.sql
@@ -3,6 +3,7 @@ SELECT
 FROM
 	(
 	SELECT
+		NULL AS "Unique ID",
 		CASE
 			WHEN s.details ILIKE '%keyword%' THEN u.northstar_id || '@dosomething.org'
 			WHEN s.details ILIKE '%broadcast%' THEN u.northstar_id || '@dosomething.org'
@@ -14,21 +15,27 @@ FROM
 			WHEN s.details ILIKE '%broadcast%' THEN NULL
 			WHEN (s.details::JSONB) ->> 'affiliateOptIn' IS NULL THEN NULL
 			ELSE u.first_name
-		END AS first_name,
+		END AS "First Name",
 		CASE
 			WHEN s.details ILIKE '%keyword%' THEN NULL
 			WHEN s.details ILIKE '%broadcast%' THEN NULL
 			WHEN (s.details::JSONB) ->> 'affiliateOptIn' IS NULL THEN NULL
 			ELSE u.last_name
-		END AS last_name,
+		END AS "Last Name",
+		NULL AS "Address1",
+		NULL AS "Address2",
+		NULL AS "City",
+		NULL AS "State",
+		NULL AS "Country",
+		u.zipcode AS "Zip",
 		CASE 
 			WHEN s.details ILIKE '%keyword%'THEN NULL
 			WHEN s.details ILIKE '%broadcast%' THEN NULL
-			ELSE (s.details::JSONB) ->> 'affiliateOptIn' 
-		END AS "NFL Reg Optin",
-		NULL AS "Site Code",
-		u.zipcode AS "Zip Code",
-		s.created_at AS "Created At"
+			WHEN (s.details::JSONB) ->> 'affiliateOptIn' = 'true' THEN 'Y'
+		END AS "NFL Reg Optin2",
+		NULL AS "NFL Reg Optin3",
+		'NULL' AS "Site Code",
+		s.created_at AS "Date Created"
 	FROM
 		signups s
 	LEFT JOIN users u ON
@@ -37,5 +44,5 @@ FROM
 		s.campaign_id = '9011'
 		AND u.email NOT ILIKE '%dosomething.org%' 
 	) nfl
-    WHERE nfl."NFL Reg Optin" = 'true'
+    WHERE nfl."NFL Reg Optin2" = 'Y'
 ;
