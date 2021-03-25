a89b64a24b7a003101f58610b9fa9cff634fe249
ghangas-ds <pghangas@dosomething.org>
2021-03-25 17:50:47 -0400

    added files from local

---
+++ Code - AdHoc Requests/2021/2021-03 avg_days_to_rb.sql
@@ -0,0 +1,26 @@
+
+-- https://dosomething.slack.com/archives/C03T8SDJG/p1615834053002900
+
+with mask_signups as
+
+	(select northstar_id, id as signup_id, created_at as signup_date
+	from public.signups where campaign_id = '9097')
+
+, mask_reportbacks as
+	(select post_action, post_created_at, northstar_id, signup_id
+		from reportbacks where campaign_id = '9097')
+
+, signup_rb_summary as
+	(select r.*
+	, s.signup_id
+	, s.signup_date
+	, date_part('day', r.post_created_at - s.signup_date) as days_to_rb
+		from mask_reportbacks r left join mask_signups s on s.signup_id = r.signup_id)
+
+, campaign_summaries as
+(select count(*) as total_reportbacks
+	, sum(days_to_rb) as total_days
+	, avg(days_to_rb)::numeric(10,2) as avg_days_to_rb
+	from signup_rb_summary)
+
+select * from campaign_summaries
