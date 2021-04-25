d8da324d3adbb0fa6a1af0bae0a36c47c43a4bb9
kkennovin <48222808+kkennovin@users.noreply.github.com>
2021-04-25 11:58:11 -0400

    Update drip_sms.sql
    - replaced total drip to post percent with buckets based on time between drip and post
    - added total number of signups and total percent of signups who post for comparison to the drip to post percent

--- Code - Analysis Projects/drip_sms.sql
+++ Code - Analysis Projects/drip_sms.sql
@@ -6,13 +6,13 @@
 -- create temp table to link broadcast_id (provided by Anthony) to campaign_id (manually searched for in public.campaign_info by Kat).
 -- this is necessary because the campaign_id field in public.messages_flattened is mostly null and not always accurate when popluated
 
---	create table analyst_sandbox.broadcast_campaign 
+--	create table analyst_sandbox.kk_broadcast_campaign 
 --		(ID serial primary key,
 --		broadcast_id varchar(255),
 --		campaign_id varchar(255)
 --		);
 --		
---	insert into analyst_sandbox.broadcast_campaign (ID, broadcast_id, campaign_id)
+--	insert into analyst_sandbox.kk_broadcast_campaign (ID, broadcast_id, campaign_id)
 --	values
 --		(1,'55vG6qkqnC6QGAeCEeuQYI',8142),
 --		(2,'4agMVK0DkkkkAOUmqweYGG',8142),
@@ -59,57 +59,155 @@ broadcast_users as
 	(select 
 		bc.broadcast_id,
 		bc.campaign_id,
-		mf.user_id as northstar_id
+		mf.user_id as northstar_id,
+		date(mf.created_at) as drip_created_at
 	from 
-		analyst_sandbox.broadcast_campaign bc
+		analyst_sandbox.kk_broadcast_campaign bc
 		left join public.messages_flattened mf
 		using (broadcast_id)
 	)
 	
--- find all users who received drip and also submitted a post
-, find_posts as 
+-- find users first post per campaign
+, first_post as 
 	(select 
-		bu.*,
-		p.id as post_id
+		bu.campaign_id,
+		bu.northstar_id,
+		min(date(p.created_at)) as post_created_at
 	from 
 		broadcast_users bu
 		left join posts p
 		using (campaign_id, northstar_id)
+	group by 
+		1, 2
+	)
+	
+-- join all users who received drip and also submitted a post
+, user_posts as 
+	(select 
+		bu.*,
+		fp.post_created_at
+	from 
+		broadcast_users bu
+		left join first_post fp
+		using (campaign_id, northstar_id)
 	)
 	
 -- join date of campaign
-, find_date as 
+, campaign_date as 
 	(select  
-		fp.*
-	,	ci.campaign_run_start_date
+		up.*
+	,	date(ci.campaign_run_start_date) as campaign_run_start_date
 	from 
-		find_posts fp
+		user_posts up
 		left join campaign_info ci
-		on cast(fp.campaign_id as int) = ci.campaign_id
+		on cast(up.campaign_id as int) = ci.campaign_id
 	)
 	
+-- calculate time between drip message and post
+, time_to_post as 
+	(select
+		cd.*,
+		case 
+			when post_created_at-drip_created_at = 0 
+			then 1 else 0
+		end as same_day,
+		case 
+			when post_created_at-drip_created_at = 1 
+			then 1 else 0
+		end as second_day,
+		case 
+			when post_created_at-drip_created_at = 2 
+			then 1 else 0
+		end as third_day,
+		case 
+			when post_created_at-drip_created_at > 2 
+			then 1 else 0
+		end as after_third_day
+	from 
+		campaign_date cd
+	)
+
 
 -- count total users who received drip and users who submitted a post
-, total_users as 
+, total_drip_users as 
 	(select 
 		campaign_id 
 	,	campaign_run_start_date
-	,	count(distinct northstar_id)::float as total_drip_users
-	,	count(post_id)::float as total_post_users
+	,	count(distinct northstar_id)::float as drip_users
+	,	count(post_created_at)::float as drip_post_users
+	,	sum(same_day)::float as total_same_day
+	,	sum(second_day)::float as total_second_day
+	,	sum(third_day)::float as total_third_day
+	,	sum(after_third_day)::float as total_after_third_day
 	from 
-		find_date fd
+		time_to_post ttp
 	group by 
 		campaign_id
 	,	campaign_run_start_date
 	order by 
 		campaign_run_start_date
 	)
+	
+-- 	find total signups and unique post users per campaign
+, find_total_users as 
+	(select 
+		tdp.campaign_id
+	,	count(distinct s.northstar_id)::float as total_signups
+	,	count(distinct p.northstar_id)::float as total_post_users
+	from 
+		total_drip_users tdp
+		left join signups s 
+		using (campaign_id)
+		left join posts p
+		using (campaign_id)
+	group by 
+		1
+	)
+	
+-- add total signups and unique post users per campaign
+, total_users as 
+	(select 
+		tdp.*
+	,	ftu.total_signups
+	,	ftu.total_post_users
+	from 
+		total_drip_users tdp
+		left join find_total_users ftu
+		using (campaign_id)
+	)
+
 
 -- calculate percentage of drip users who complete a post
 select 
-	tu.*
-,	(total_post_users/total_drip_users)*100 as drip_post_percent
+	campaign_id
+,	campaign_run_start_date
+,	total_signups
+,	total_post_users
+,	drip_users
+,	drip_post_users
+,	round((total_post_users/total_signups)*100, 2) as total_post_percent
+,	round((drip_post_users/drip_users)*100, 2) as drip_post_percent
+,	case
+		when drip_post_users > 0 
+		then round((total_same_day/drip_post_users)*100, 2)
+		else 0 
+	end as same_day_percent
+,	case 
+		when drip_post_users > 0 
+		then round((total_second_day/drip_post_users)*100, 2)
+		else 0 
+	end as second_day_percent
+,	case 
+		when drip_post_users > 0 
+		then round((total_third_day/drip_post_users)*100, 2)
+		else 0 
+	end as third_day_percent
+,	case 
+		when drip_post_users > 0 
+		then round((total_after_third_day/drip_post_users)*100, 2)
+		else 0 
+	end as after_third_day_percent
 from 
 	total_users tu
 	
-	
+
